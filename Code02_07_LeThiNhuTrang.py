82dfd1d43232a3027b0f5c9786e6af06dc2790f0
ThuHa96 <80746876+ThuHa96@users.noreply.github.com>
2021-04-07 18:00:03 +0900

    Face mask detection
    Face mask detection

---
+++ Code02_07_LeThiNhuTrang.py
@@ -0,0 +1,18 @@
+import turtle
+
+t = turtle.Pen()
+t.pencolor('red')
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+import turtle
+
+t=turtle.Turtle()
+for i in [0, 1, 2, 3]:
+    t.forward(50)
+    t.right(90)
